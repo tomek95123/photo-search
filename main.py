f9a0b8d81de06fdbcb1ec808523f59d8a17b7192
Tomfa <tomekfaber@onet.pl>
2023-05-14 22:02:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+import wikipedia
+import requests
+
+Builder.load_file('frontend.kv')
+
+class FirstScreen(Screen):
+    def get_image_link(self):
+
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and first image link
+        page = wikipedia.page(query, auto_suggest=False)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        # Download image
+        headers = {'User-agent': 'Mozilla/5.0'}
+        req = requests.get(self.get_image_link(), headers=headers)
+        imagepath = 'files/image.png'
+        with open(imagepath, 'wb') as file:
+            file.write(req.content)
+        return imagepath
+
+    def set_image(self):
+        # Set the image in the image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
